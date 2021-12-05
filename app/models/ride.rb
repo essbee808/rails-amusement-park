b7a2e87f93c1356c9dcd73335786de0b7a73beb9
essbee808 <sheena.myb@gmail.com>
2021-12-04 17:09:57 -0800

    Create take_ride method and modify conditional

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,14 @@
 class Ride < ActiveRecord::Base
   belongs_to :user
   belongs_to :attraction
+
+  def take_ride
+    
+    #user doesn't have enough tickets
+     #Sorry,
+    #user not being tall enough
+    #user doesn't have enough tickets and isn't tall enough
+    #Updates ticket number
+    #Updates user's nausea
+  end
 end
