681b6c97ae018ac721b107999cf572a3ce5204cd
essbee808 <sheena.myb@gmail.com>
2021-12-04 17:19:11 -0800

    Create users controller and root path

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -5,10 +5,16 @@ class Ride < ActiveRecord::Base
   def take_ride
     
     #user doesn't have enough tickets
-     #Sorry,
+     #Sorry. You do not have enough tickets to ride the #{attraction.name}.
     #user not being tall enough
+     #Sorry. You are not tall enough to ride the #{attraction.name}
     #user doesn't have enough tickets and isn't tall enough
+     #Sorry. You do not have enough tickets to ride the #{attraction.name}.
+     # You are not tall enough to ride the #{attraction.name}.
+
     #Updates ticket number
     #Updates user's nausea
+
+    
   end
 end
